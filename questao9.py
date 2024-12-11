6c6b357e239f6c8aa3036bdbb1bcadbef38a39ff
joaoallander <joaoallander0@gmail.com>
2024-12-11 12:40:53 -0300

    commit

---
+++ questao9.py
@@ -0,0 +1,10 @@
+print("BEM VENDO AO CONTADOR GWA \n")
+
+
+n1=float(input("ola, bem vindo!!, insira o valor inicial da contagem \n"))
+n=float(input("agora insira o valor final da contagem \n"))
+while(n1<n):
+    print(n1)
+    n1=n1+1
+    
+
